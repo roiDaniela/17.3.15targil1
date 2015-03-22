c16fbfdb0334c8daed716de48ad768b4f77d46fb
Motty Katz <mottyk85@gmail.com>
2015-03-22 22:01:34 +0200

    22.3.15 - 4

---
+++ Exer1/ScreenData.cpp
@@ -0,0 +1,11 @@
+
+#include "ScreenData.h"
+
+
+bool ScreenData::is_point_exist(Point& ptPoint)
+{
+
+}
+
+bool insert_point(Point & ptPoint);
+void clear_data();
--- Exer1/ScreenData.h
+++ Exer1/ScreenData.h
@@ -2,15 +2,18 @@
 #define _SCREENDATA_H_
 #include <list>
 #include "Point.h"
+
 class ScreenData
 {
-	std::list< int > Data;
+
+private:
+	std::list< Point > Data;
 	
-
-
+public:
 	bool is_point_exist(Point& ptPoint);
 	bool insert_point(Point & ptPoint);
 	void clear_data();
+
 };
 
 #endif
