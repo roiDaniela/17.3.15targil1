12799ae77fd908c686315a78bb0d61cc2139742a
roiDaniela <roi.fogler@gmail.com>
2015-03-22 21:56:51 +0200

    commit point.h 22.3.15

--- Exer1/Point.h
+++ Exer1/Point.h
@@ -7,13 +7,19 @@ using namespace std;
 class Point
 {
 private:	
-	int x,y; 
+	unsigned int x,y; 
 public:
-	Point(int x, int y){ this->x = x; this->y = y; }
-	int getX(){ return x; };
-	void setX(int x){ this->x = x; };
-	int getY(){ return y; };
-	void setY(int y){ this->y = y; };
+	//Ctor
+	Point(unsigned int x, unsigned int y){ this->x = x; this->y = y; }
+	
+	// Getter & setter
+	unsigned getX(){ return x; };
+	void setX(unsigned int x){ this->x = x; };
+	unsigned int getY(){ return y; };
+	void setY(unsigned int y){ this->y = y; };
+
+	// Methods
+	Point randomPoint(unsigned target, unsigned int start = 1){ rand() % target + start; }
 };
 
 #endif
